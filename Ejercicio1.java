2462079ebceeb5b38ebe9ad292c4fbe6a3e27684
SokiSotos <sokisotos@hotmail.com>
2022-10-10 11:59:27 +0200

    Ej1 - Mi Nombre

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,9 +1,5 @@
 public class Ejercicio1 {
-    public static void main(String [] args) {
-<<<<<<< HEAD
-        System.out.println("Mi Nombre es: Jose Sotos Perez");
-=======
-        System.out.println("Hola Mundo");
->>>>>>> 84c8b2d9b31f9472d08e5a46576ac1356dd66250
+    public static void main (String [] args) {
+        System.out.println("Mi nombre es: Jose Sotos Perez");
     }
 }
