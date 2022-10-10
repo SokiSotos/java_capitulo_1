141007ea86cf1ceaed754d384fe7edee5b1b551a
SokiSotos <sokisotos@hotmail.com>
2022-10-10 12:37:34 +0200

    Ej4 - Horario

---
+++ Ejercicio4.java
@@ -0,0 +1,19 @@
+public class Ejercicio4 {
+    public static void main (String [] args) {
+        System.out.println("┌───────────┬───────────┬───────────┬───────────┬───────────┐");
+        System.out.println("│   Lunes   │   Martes  │ Miercoles │   Jueves  │  Viernes  │");
+        System.out.println("├───────────┼───────────┼───────────┼───────────┼───────────┤");
+        System.out.println("│   PROGR   │   SISTM   │   ENDES   │   PROGR   │    FOL    │");
+        System.out.println("├───────────┼───────────┼───────────┼───────────┼───────────┤");
+        System.out.println("│   PROGR   │   SISTM   │   ENDES   │   PROGR   │    FOL    │");
+        System.out.println("├───────────┼───────────┼───────────┼───────────┼───────────┤");
+        System.out.println("│   LMSGI   │   SISTM   │   PROGR   │   LMSGI   │    FOL    │");
+        System.out.println("├───────────┼───────────┼───────────┼───────────┼───────────┤");
+        System.out.println("│   LMSGI   │   PROGR   │   PROGR   │   LMSGI   │   SISTM   │");
+        System.out.println("├───────────┼───────────┼───────────┼───────────┼───────────┤");
+        System.out.println("│   BADAT   │   PROGR   │   PROGR   │   BADAT   │   SISTM   │");
+        System.out.println("├───────────┼───────────┼───────────┼───────────┼───────────┤");
+        System.out.println("│   BADAT   │   ENDES   │   BADAT   │   BADAT   │   SISTM   │");
+        System.out.println("└───────────┴───────────┴───────────┴───────────┴───────────┘");
+    }
+}
